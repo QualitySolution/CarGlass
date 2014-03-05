1be467967bf2cfa3a7f1bf081c0bab41de084cd2
Andrey Gankov <gav@qsolution.ru>
2014-03-05 16:00:16 +0400

    На весь экран при старте.

--- CarGlass/MainWindow.cs
+++ CarGlass/MainWindow.cs
@@ -10,6 +10,7 @@ public partial class MainWindow: Gtk.Window
 	public MainWindow() : base(Gtk.WindowType.Toplevel)
 	{
 		Build();
+		Maximize();
 
 		//Передаем лебл
 		MainClass.StatusBarLabel = labelStatus;
