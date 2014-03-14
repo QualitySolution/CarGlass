e1298aeec551f4ddf3649e7095b91c0bbc88c1d2
Andrey Gankov <gav@qsolution.ru>
2014-03-14 13:46:52 +0400

    Правила удаления

--- CarGlass/Program.cs
+++ CarGlass/Program.cs
@@ -71,21 +71,108 @@ namespace CarGlass
 			TableInfo PrepareTable;
 
 			PrepareTable = new TableInfo();
-			PrepareTable.ObjectsName = "Места";
-			PrepareTable.ObjectName = "место";
-			PrepareTable.SqlSelect = "SELECT place_types.name as type, place_no, area , type_id FROM places " +
-			"LEFT JOIN place_types ON places.type_id = place_types.id ";
-			PrepareTable.DisplayString = "Место {0}-{1} с площадью {2} кв.м.";
-			PrepareTable.PrimaryKey = new  TableInfo.PrimaryKeys("type_id", "place_no");
-			PrepareTable.DeleteItems.Add("contracts", 
-				new TableInfo.DeleteDependenceItem("WHERE contracts.place_type_id = @type_id AND contracts.place_no = @place_no", "@place_no", "@type_id"));
-			PrepareTable.DeleteItems.Add("meters", 
-				new TableInfo.DeleteDependenceItem("WHERE meters.place_type_id = @type_id AND meters.place_no = @place_no ", "@place_no", "@type_id"));
-			PrepareTable.DeleteItems.Add("events", 
-				new TableInfo.DeleteDependenceItem("WHERE place_type_id = @type_id AND place_no = @place_no AND lessee_id IS NULL", "@place_no", "@type_id"));
-			PrepareTable.ClearItems.Add("events", 
-				new TableInfo.ClearDependenceItem("WHERE place_type_id = @type_id AND place_no = @place_no AND lessee_id IS NOT NULL", "@place_no", "@type_id", "place_type_id", "place_no"));
-			Tables.Add("places", PrepareTable);
+			PrepareTable.ObjectsName = "Пользователи";
+			PrepareTable.ObjectName = "пользователя"; 
+			PrepareTable.SqlSelect = "SELECT name, id FROM users ";
+			PrepareTable.DisplayString = "{0}";
+			PrepareTable.PrimaryKey = new TableInfo.PrimaryKeys("id");
+			Tables.Add ("users", PrepareTable);
+
+			PrepareTable = new TableInfo();
+			PrepareTable.ObjectsName = "Услуги";
+			PrepareTable.ObjectName = "услуга";
+			PrepareTable.SqlSelect = "SELECT name , id FROM services ";
+			PrepareTable.DisplayString = "Услуга {0}";
+			PrepareTable.PrimaryKey = new  TableInfo.PrimaryKeys("id");
+			PrepareTable.DeleteItems.Add("order_pays", 
+				new TableInfo.DeleteDependenceItem ("WHERE service_id = @id ", "", "@id"));
+			Tables.Add("services", PrepareTable);
+
+			PrepareTable = new TableInfo();
+			PrepareTable.ObjectsName = "Оплаты заказа";
+			PrepareTable.ObjectName = "оплата заказа";
+			PrepareTable.SqlSelect = "SELECT order_id, id, cost FROM order_pays ";
+			PrepareTable.DisplayString = "Оплата в заказе {0} на сумму{2:C}";
+			PrepareTable.PrimaryKey = new  TableInfo.PrimaryKeys("id");
+			Tables.Add("order_pays", PrepareTable);
+
+			PrepareTable = new TableInfo();
+			PrepareTable.ObjectsName = "Заказы";
+			PrepareTable.ObjectName = "заказ";
+			PrepareTable.SqlSelect = "SELECT id, date, hour FROM orders ";
+			PrepareTable.DisplayString = "Заказ №{0} на {2} часа {1:d} числа";
+			PrepareTable.PrimaryKey = new  TableInfo.PrimaryKeys("id");
+			Tables.Add("orders", PrepareTable);
+
+			PrepareTable = new TableInfo();
+			PrepareTable.ObjectsName = "Стекла в заказе";
+			PrepareTable.ObjectName = "стекло в заказе";
+			PrepareTable.SqlSelect = "SELECT order_id, id FROM order_glasses ";
+			PrepareTable.DisplayString = "Стекло в заказе №{0}";
+			PrepareTable.PrimaryKey = new  TableInfo.PrimaryKeys("id");
+			Tables.Add("order_glasses", PrepareTable);
+
+			PrepareTable = new TableInfo();
+			PrepareTable.ObjectsName = "Виды стекол";
+			PrepareTable.ObjectName = "вид стекла";
+			PrepareTable.SqlSelect = "SELECT name, id FROM glass ";
+			PrepareTable.DisplayString = "Стекло {0}";
+			PrepareTable.PrimaryKey = new  TableInfo.PrimaryKeys("id");
+			PrepareTable.DeleteItems.Add("order_glasses", 
+				new TableInfo.DeleteDependenceItem ("WHERE glass_id = @id ", "", "@id"));
+			Tables.Add("glass", PrepareTable);
+
+			PrepareTable = new TableInfo();
+			PrepareTable.ObjectsName = "Статусы";
+			PrepareTable.ObjectName = "статус";
+			PrepareTable.SqlSelect = "SELECT name, id FROM status ";
+			PrepareTable.DisplayString = "Статус {0}";
+			PrepareTable.PrimaryKey = new  TableInfo.PrimaryKeys("id");
+			PrepareTable.DeleteItems.Add("orders", 
+				new TableInfo.DeleteDependenceItem ("WHERE status_id = @id ", "", "@id"));
+			Tables.Add("status", PrepareTable);
+
+			PrepareTable = new TableInfo();
+			PrepareTable.ObjectsName = "Склады";
+			PrepareTable.ObjectName = "склад";
+			PrepareTable.SqlSelect = "SELECT name, id FROM stocks ";
+			PrepareTable.DisplayString = "Склад {0}";
+			PrepareTable.PrimaryKey = new  TableInfo.PrimaryKeys("id");
+			PrepareTable.ClearItems.Add ("orders", 
+				new TableInfo.ClearDependenceItem ("WHERE stock_id = @id", "", "@id", "stock_id"));
+			Tables.Add("stocks", PrepareTable);
+
+			PrepareTable = new TableInfo();
+			PrepareTable.ObjectsName = "Производители";
+			PrepareTable.ObjectName = "производитель";
+			PrepareTable.SqlSelect = "SELECT name, id FROM manufacturers ";
+			PrepareTable.DisplayString = "Производитель {0}";
+			PrepareTable.PrimaryKey = new  TableInfo.PrimaryKeys("id");
+			PrepareTable.ClearItems.Add ("orders", 
+				new TableInfo.ClearDependenceItem ("WHERE manufacturer_id = @id", "", "@id", "manufacturer_id"));
+			Tables.Add("manufacturers", PrepareTable);
+
+			PrepareTable = new TableInfo();
+			PrepareTable.ObjectsName = "Модели автомобилей";
+			PrepareTable.ObjectName = "модель автомобиля";
+			PrepareTable.SqlSelect = "SELECT marks.name, models.name, models.id FROM models " +
+				"LEFT JOIN marks ON marks.id = models.mark_id ";
+			PrepareTable.DisplayString = "Модель {0} {1}";
+			PrepareTable.PrimaryKey = new  TableInfo.PrimaryKeys("id");
+			PrepareTable.DeleteItems.Add("orders", 
+				new TableInfo.DeleteDependenceItem ("WHERE car_model_id = @id ", "", "@id"));
+			Tables.Add("models", PrepareTable);
+
+			PrepareTable = new TableInfo();
+			PrepareTable.ObjectsName = "Марки автомобилей";
+			PrepareTable.ObjectName = "марка автомобиля";
+			PrepareTable.SqlSelect = "SELECT name, id FROM marks ";
+			PrepareTable.DisplayString = "Марка {0}";
+			PrepareTable.PrimaryKey = new  TableInfo.PrimaryKeys("id");
+			PrepareTable.DeleteItems.Add("models", 
+				new TableInfo.DeleteDependenceItem ("WHERE mark_id = @id ", "", "@id"));
+			Tables.Add("marks", PrepareTable);
+
 		}
 
 		public static void StatusMessage(string message)
