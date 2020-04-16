def84380eb10e4f9a6a1fc5c7d1c7b09473bd2fa
Soldatkina-Alina <alinasoldatkina2014@gmail.com>
2020-04-16 15:58:34 +0300

    Добавлена проверка на удаление коэффициента

--- CarGlass/Dialogs/AddEditFormulas.cs
+++ CarGlass/Dialogs/AddEditFormulas.cs
@@ -7,6 +7,7 @@ using Gtk;
 using QS.Dialog.Gtk;
 using QS.DomainModel.UoW;
 using QSOrmProject;
+using QSProjectsLib;
 
 namespace CarGlass.Dialogs
 {
@@ -17,7 +18,7 @@ namespace CarGlass.Dialogs
 		Coefficients coefficients = null;
 		IList<Domain.SalaryFormulas> listSalaryFormulas;
 		IList<Coefficients> listCoefficients;
-
+		IList<Coefficients> listCoefficientsDelete = new List<Coefficients>();
 
 		public AddEditFormulas()
 		{
@@ -53,9 +54,43 @@ namespace CarGlass.Dialogs
 		protected void OnBtnDeleteCoeffClicked(object sender, EventArgs e)
 		{
 			Coefficients coeff = ytreeCoeff.GetSelectedObject<Coefficients>();
+			if(checkCoeffBeforeDelete(coeff.Name))
+			{
+				MessageDialogWorks.RunWarningDialog("Коэффициент присутствует в формулах.\n Удаление невозможно.");
+				return;
+			}
 			listCoefficients.Remove(coeff);
+			listCoefficientsDelete.Add(coeff);
 			ytreeCoeff.ItemsDataSource = listCoefficients;
 		}
+
+		private bool checkCoeffBeforeDelete(string coeff)
+		{
+			Domain.SalaryFormulas salaryFormulas = null;
+			IList<Domain.SalaryFormulas> listFormulas = UoW.Session.QueryOver<Domain.SalaryFormulas>(() => salaryFormulas).List();
+			foreach(var formula in listFormulas)
+				if(formula.Formula.Contains(coeff))
+				{
+					string str = "- =/*+()";
+					int index = formula.Formula.IndexOf(coeff);
+					int indexAfter = index + coeff.Length + 1;
+
+					string strAfter, strBefore;
+					if(indexAfter > formula.Formula.Length)
+						strAfter = " ";
+					else strAfter = formula.Formula.ToArray()[indexAfter].ToString();
+					if(index == 0)
+						strBefore = " ";
+					else strBefore = formula.Formula.ToArray()[--index].ToString();
+
+					if(str.Contains(strBefore) && str.Contains(strAfter))
+						return true;// удалять нельзя
+					return false;
+				}
+
+			return false;
+		}
+
 
 		protected void OnButtonOkClicked(object sender, EventArgs e)
 		{
