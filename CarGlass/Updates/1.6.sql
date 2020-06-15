8087765c243094a9911c212b805ccd2b4a3e9d9b
Soldatkina-Alina <alinasoldatkina2014@gmail.com>
2020-06-15 18:19:36 +0300

    Добавлены скрипты для создания или обновления таблиц

--- CarGlass/Updates/1.6.sql
+++ CarGlass/Updates/1.6.sql
@@ -1,4 +1,71 @@
-﻿-- В заказы добавить order_type
+﻿-- -----------------------------------------------------
+-- Table `CarGlass`.`order_types`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `CarGlass`.`order_type` (
+ `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `is_calculate_salary` BOOLEAN NOT NULL DEFAULT 0,
+  `position_in_tabs` varchar(1000) NULL DEFAULT null,
+  `is_show_main_widgets` BOOLEAN NOT NULL DEFAULT 1,
+  `is_show_additional_widgets` BOOLEAN NOT NULL DEFAULT 0,
+  `is_install_type` BOOLEAN NOT NULL DEFAULT 0,
+  `is_other_type` BOOLEAN NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+-- -----------------------------------------------------
+-- Table `CarGlass`.`service_order_type`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `CarGlass`.`service_order_type` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_service` INT(10) UNSIGNED NOT NULL,
+  `id_type_order` INT(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_service_order_type_1_idx` (`id_service` ASC),
+  INDEX `fk_service_order_type_2_idx` (`id_type_order` ASC),
+  CONSTRAINT `fk_service_order_type_1`
+    FOREIGN KEY (`id_service`)
+    REFERENCES `CarGlass`.`services` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_service_order_type_2`
+    FOREIGN KEY (`id_type_order`)
+    REFERENCES `CarGlass`.`order_type` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+-- -----------------------------------------------------
+-- Alter table `CarGlass`.`orders`
+-- -----------------------------------------------------
+
+ALTER TABLE `CarGlass`.`orders` 
+ADD COLUMN `id_order_type` INT(10) UNSIGNED NULL DEFAULT NULL AFTER `comment`,
+ADD INDEX `fk_order_type_idx` (`id_order_type` ASC);
+ALTER TABLE `CarGlass`.`orders` 
+ADD CONSTRAINT `fk_order_type`
+  FOREIGN KEY (`id_order_type`)
+  REFERENCES `CarGlass`.`order_type` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+-- -----------------------------------------------------
+-- Alter table `CarGlass`.`note`
+-- -----------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS `CarGlass`.`note` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `date` DATETIME NOT NULL,
+  `point_number` TINYINT(4) NOT NULL,
+  `calendar_number` TINYINT(4) NOT NULL,
+  `message` VARCHAR(2000) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+-- В заказы добавить order_type
 INSERT INTO order_type (name, is_calculate_salary, position_in_tabs, is_show_main_widgets,
 is_show_additional_widgets, is_install_type, is_other_type) 
 VALUES( 'Установка стекол', false, 'Установка пригородный Заказные', true, true, true, false);
