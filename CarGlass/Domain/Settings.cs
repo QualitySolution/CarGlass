d1233e158c38442d051cb520a4e04dfa1e818ebb
Soldatkina-Alina <alinasoldatkina2014@gmail.com>
2020-09-03 17:27:53 +0300

    Добавлены классы для настроек

---
+++ CarGlass/Domain/Settings.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Bindings.Collections.Generic;
+using QS.DomainModel.Entity;
+
+namespace CarGlass.Domain
+{
+	public class Settings : PropertyChangedBase, IDomainObject
+	{
+		public virtual int Id { get; set; }
+
+		private string parametr;
+		[Display(Name = "Название")]
+		public virtual string Parametr
+		{
+			get { return parametr; }
+			set { SetField(ref parametr, value); }
+		}
+
+		private string description;
+		[Display(Name = "Описание")]
+		public virtual string Description
+		{
+			get { return description; }
+			set { SetField(ref description, value); }
+		}
+
+		private string value;
+		[Display(Name = "Значение")]
+		public virtual string Value
+		{
+			get { return value; }
+			set { SetField(ref value, value); }
+		}
+
+		private DateTime dateEdit;
+		[Display(Name = "Дата изменения")]
+		public virtual DateTime DateEdit
+		{
+			get { return dateEdit; }
+			set { SetField(ref dateEdit, value); }
+		}
+
+		public Settings()
+		{
+		}
+
+	}
+}
---
+++ CarGlass/HibernateMapping/SettingsMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CarGlass.Domain;
+using FluentNHibernate.Mapping;
+
+namespace CarGlass.HibernateMapping
+{
+	public class SettingsMap : ClassMap<Settings>
+	{
+		public SettingsMap()
+		{
+			Table("settings");
+
+			Id(x => x.Id).Column("id").GeneratedBy.Native();
+			Map(x => x.Parametr).Column("parametr");
+			Map(x => x.Value).Column("value");
+			Map(x => x.Description).Column("description");
+			Map(x => x.DateEdit).Column("date_edit");
+		}
+	}
+}
