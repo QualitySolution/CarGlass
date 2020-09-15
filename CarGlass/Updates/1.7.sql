9543b82fff778802339e0ab94546f5bc3449a989
Soldatkina-Alina <alinasoldatkina2014@gmail.com>
2020-09-15 09:50:43 +0300

    Изменения для БД

---
+++ CarGlass/Updates/1.7.sql
@@ -0,0 +1,24 @@
+﻿-- -----------------------------------------------------
+-- Table `CarGlass`.`settings`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `settings` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,  
+  `parametr` VARCHAR(45) NOT NULL,
+  `value` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(45) NULL DEFAULT NULL,
+  `date_edit` DATETIME NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+-- -----------------------------------------------------
+-- Alter table `CarGlass`.`users`
+-- -----------------------------------------------------
+
+ALTER TABLE `users` 
+ADD COLUMN `manager` `manager` TINYINT(1) NULL DEFAULT FALSE ,
+ADD COLUMN `worker` `worker` TINYINT(1) NULL DEFAULT FALSE ;
+
+
+DELETE FROM base_parameters WHERE name = 'micro_updates';
+UPDATE base_parameters SET str_value = '1.7' WHERE name = 'version';
