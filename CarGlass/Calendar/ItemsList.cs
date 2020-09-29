7df934b4b535271f604d985cbc2449029cfe1010
Soldatkina-Alina <alinasoldatkina2014@gmail.com>
2020-09-29 17:03:34 +0300

    Добавлен новый класс для работы с CalendarItem

---
+++ CarGlass/Calendar/ItemsList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarGlass.Calendar
+{
+	public class ItemsList
+	{
+		public readonly List<CalendarItem>[,] TimeMap = new List<CalendarItem>[7, 24];
+
+		public ItemsList()
+		{
+		}
+
+		public void AddItem(int day, int hour, CalendarItem item)
+		{
+			if(day > 6 || day < 0) return;
+			if(TimeMap[day, hour] == null)
+				TimeMap[day, hour] = new List<CalendarItem>();
+			TimeMap[day, hour].Add(item);
+		}
+	}
+}
