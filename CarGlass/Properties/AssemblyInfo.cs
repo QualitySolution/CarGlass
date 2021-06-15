952bcfd53fb378c7a347d82828b1cc675ed6c283
Gankov <gav@qsolution.ru>
2021-06-15 11:26:33 +0300

    1.8 и обновление атрибутов проекта.

--- CarGlass/Properties/AssemblyInfo.cs
+++ CarGlass/Properties/AssemblyInfo.cs
@@ -1,31 +1,33 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using QSProjectsLib;
+using QS.Project.Versioning;
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
 
 [assembly: AssemblyTitle("QS: Мастерская автостекла")]
+#if DEBUG
+[assembly: AssemblyBetaBuild]
+#endif
 [assembly: AssemblyDescription("Регистрация заказов в мастерской автостекл Стеклоff.")]
-[assembly: AssemblyEdition ("gpl")]
+[assembly: AssemblyСompatibleModification("gpl")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Quality Solution")]
 [assembly: AssemblyProduct("QS: Мастерская автостекла")]
-[assembly: AssemblyCopyright("Quality Solution 2020")]
+[assembly: AssemblyCopyright("Quality Solution 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyLogoIcon ("CarGlass.icons.logo.png")]
-[assembly: AssemblySupport ("", true, "MySQL")]
-[assembly: AssemblyAppWebsite ("http://develop.qsolution.ru/")]
+[assembly: AssemblyLogoIcon("CarGlass.icons.logo.png")]
+[assembly: AssemblySupport("Телефон тех. поддержки +7(812)309-71-77")]
+[assembly: AssemblyAppWebsite("http://develop.qsolution.ru/")]
 
-[assembly: AssemblyAuthor ("Ганьков Андрей <gav@qsolution.ru>")]
-[assembly: AssemblyAuthor("Солдаткина Алина <alina@qsolution.ru>")]
+[assembly: AssemblyAuthor("Ганьков Андрей <gav@qsolution.ru>")]
+[assembly: AssemblyAuthor("Солдаткина Алина 2020 <alina@qsolution.ru>")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion("1.7.9")]
+[assembly: AssemblyVersion("1.8")]
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
