7ed69621bdb14a96798eeac89160a048ed22d130
Gankov <gav@qsolution.ru>
2021-06-15 18:03:18 +0300

    Доменная модель и таблицы в базе по отправке СМС.

---
+++ CarGlass/Domain/SMS/Message.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using QS.DomainModel.Entity;
+
+namespace CarGlass.Domain.SMS
+{
+	public class Message : PropertyChangedBase, IDomainObject
+	{
+		public virtual int Id { get; set; }
+
+		private WorkOrder workOrder;
+		[Display(Name = "Заказ")]
+		public virtual WorkOrder WorkOrder
+		{
+			get => workOrder;
+			set => SetField(ref workOrder, value);
+		}
+
+		private string phone;
+		[Display(Name = "Телефон")]
+		public virtual string Phone
+		{
+			get => phone;
+			set => SetField(ref phone, value);
+		}
+
+		private string messageId;
+		[Display(Name = "ID Сообщения")]
+		public virtual string MessageId
+		{
+			get => messageId;
+			set => SetField(ref messageId, value);
+		}
+
+		private DateTime sentTime;
+		[Display(Name = "Время отправки")]
+		public virtual DateTime SentTime
+		{
+			get => sentTime;
+			set => SetField(ref sentTime, value);
+		}
+
+		private string lastStatus;
+		[Display(Name = "Последний состояние")]
+		public virtual string LastStatus
+		{
+			get => lastStatus;
+			set => SetField(ref lastStatus, value);
+		}
+
+		private DateTime? lastStatusTime;
+		[Display(Name = "Время получения последнего состояния")]
+		public virtual DateTime? LastStatusTime
+		{
+			get => lastStatusTime;
+			set => SetField(ref lastStatusTime, value);
+		}
+
+		private string text;
+		[Display(Name = "Сообщение")]
+		public virtual string Text
+		{
+			get => text;
+			set => SetField(ref text, value);
+		}
+
+	}
+}
---
+++ CarGlass/HibernateMapping/SMS/MessageMap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CarGlass.Domain.SMS;
+using FluentNHibernate.Mapping;
+
+namespace CarGlass.HibernateMapping.SMS
+{
+	public class MessageMap : ClassMap<Message>
+	{
+		public MessageMap()
+		{
+			Table("sms_history");
+
+			Id(x => x.Id).Column("id").GeneratedBy.Native();
+			Map(x => x.Phone).Column("phone");
+			Map(x => x.MessageId).Column("message_id");
+			Map(x => x.SentTime).Column("sent_time");
+			Map(x => x.LastStatus).Column("last_status");
+			Map(x => x.LastStatusTime).Column("last_status_time");
+			Map(x => x.Text).Column("text");
+
+			References(x => x.WorkOrder).Column("order_id");
+		}
+	}
+}
