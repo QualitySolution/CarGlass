601febc0e4790cf58a6209fba7223e023e5acc3d
Gankov <gav@qsolution.ru>
2025-11-07 14:54:55 +0300

    Обновил конфигурацию апдейтера.

--- CarGlass/ConfigureUpdates.cs
+++ CarGlass/ConfigureUpdates.cs
@@ -14,7 +14,7 @@ namespace CarGlass
 				new Version(1, 4),
 				"CarGlass.Updates.1.4.sql");
 
-			configuration.AddMicroUpdate(
+			configuration.AddUpdate(
 				new Version(1, 4),
 				new Version(1, 4, 2),
 				"CarGlass.Updates.1.4.2.sql");
@@ -24,12 +24,12 @@ namespace CarGlass
 				new Version(1, 5),
 				"CarGlass.Updates.1.5.sql");
 
-			configuration.AddMicroUpdate(
+			configuration.AddUpdate(
 				new Version(1, 5),
 				new Version(1, 5, 1),
 				"CarGlass.Updates.1.5.1.sql");
 
-			configuration.AddMicroUpdate(
+			configuration.AddUpdate(
 				new Version(1, 5, 1),
 				new Version(1, 5, 2),
 				"CarGlass.Updates.1.5.2.sql");
@@ -44,7 +44,7 @@ namespace CarGlass
 				new Version(1, 7),
 				"CarGlass.Updates.1.7.sql");
 
-			configuration.AddMicroUpdate(
+			configuration.AddUpdate(
 				new Version(1, 7),
 				new Version(1, 7, 3),
 				"CarGlass.Updates.1.7.3.sql");
@@ -54,7 +54,7 @@ namespace CarGlass
 				new Version(1, 8),
 				"CarGlass.Updates.1.8.sql");
 
-			configuration.AddMicroUpdate(
+			configuration.AddUpdate(
 				new Version(1, 8),
 				new Version(1, 8, 2),
 				"CarGlass.Updates.1.8.2.sql");
